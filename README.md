52f0247cbfaa7ae98396a5cbf94b9955c48d6272
Johannes Elmnäinen <sanhologin@gmail.com>
2019-12-01 22:29:40 +0200

    Add a blanker

--- README.md
+++ README.md
@@ -7,6 +7,18 @@ First, set `MAP_APP_API_KEY` to your Google Maps Platform API key:
 export MAP_APP_API_KEY=<your-api-key>
 ```
 
+Then, create file `./magicWords.js` which exports a list of magic words that open the map. Here is an example file:
+```
+//magicWords.js
+
+let magicWords = [
+  'abradacabra',
+  'seesam aukene'
+]
+
+export default magicWords
+
+```
 Then, start webpack-dev-server by running
 ```
 npm run dev
--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 import './index.css'
-console.log('index.js loaded :)')
+import magicWords from '../magicWords'
+
+const magicWordTransform = (word) =>
+  word
+    .toLowerCase()
+    .replace(/i am/g, "i m")
+    .replace(/ /g, "")
+    .replace(/'/g, "")
+    .replace(/,/g, "")
+
+
+let magic_words = magicWords.map(magicWordTransform)
+
+window.onload = function() {
+
+  const blanker = document.getElementById('map-blanker')
+  function testChange(e) {
+    const val = magicWordTransform(blanker.textContent)
+
+    for (let word in magic_words) {
+      if (val == magic_words[word]) {
+        const mapElem = document.getElementById('map')
+        mapElem.classList.remove("hidden")
+        blanker.classList.add("hidden")
+      }
+    }
+  }
+
+
+  blanker.oninput = testChange
+}
