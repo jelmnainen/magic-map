2bf1a659059a4dbeb3af0c8e1d9287e4a86d3f21
Johannes Elmnäinen <sanhologin@gmail.com>
2019-12-27 14:24:27 +0200

    init control plane

---
+++ control-plane/webpack.dev.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = env => ({
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: 'dist',
+    proxy: {
+      '/api': {
+        'target': 'http://localhost:3001',
+        'pathRewrite': {'^/api': ''}
+      }
+    }
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: 'head',
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ]
+});
