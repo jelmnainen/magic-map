69b1d85835969b358365ff1520c7824bead76ff0
Johannes Elmnäinen <sanhologin@gmail.com>
2020-02-14 18:15:35 +0200

    set cookie on correct map password and skip asking password for 24h

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,6 +21,28 @@ const magicWordTransform = (word) =>
     .replace(/'/g, "")
     .replace(/,/g, "")
 
+const setCookie = (key, value) => {
+  let d = new Date();
+  d.setTime(d.getTime() + (1 * 24 * 60 * 60 * 1000));
+  const expires = "expires="+d.toUTCString();
+  document.cookie = key + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return null;
+}
+
 const testForMagicWords = (blankerElem) => (e) => {
   const val = magicWordTransform(blankerElem.textContent)
 
@@ -29,6 +51,7 @@ const testForMagicWords = (blankerElem) => (e) => {
       const mapElem = document.getElementById('map')
       mapElem.classList.remove("hidden")
       blankerElem.classList.add("hidden")
+      setCookie('hadCorrectPassword', 'true')
       // IF you want to be a nice person comment this
       // setTimeout(tryToHideExtraMapStuff, 600)
     }
@@ -41,7 +64,7 @@ const magic_words = magicWords.map(magicWordTransform)
 
 let activeMarker = {}
 
-const devShowMap = () => {
+const skipBlanker = () => {
   const blankerElem = document.getElementById('map-blanker')
   const mapElem = document.getElementById('map')
   mapElem.classList.remove("hidden")
@@ -80,8 +103,9 @@ window.onload = function() {
   const blanker = document.getElementById('map-blanker')
   blanker.oninput = testForMagicWords(blanker)
 
-  //remove this in production
-  // devShowMap()
+  if (getCookie('hadCorrectPassword') === 'true') {
+    skipBlanker()
+  }
 
   //setup map
   initMap('map')
